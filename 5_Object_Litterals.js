faaab0b37dfe1aba3c64c50684d0c2724af8123b
Muhammadirfan695 <82946340+Muhammadirfan695@users.noreply.github.com>
2023-08-31 11:48:01 +0500

    Add files via upload

--- 5_Object_Litterals.js
+++ 5_Object_Litterals.js
@@ -58,6 +58,7 @@ console.log(obj.details())  // object declare with function
 
 */
 
+/*
 // ----------- 5th Method object with function declare Method with ES6 version declare function Method -----------
 let n = "student";
 let  obj = {
@@ -68,6 +69,62 @@ let  obj = {
        return `This is Name "${this.student_name}" course name "${this.course}"`
    }    
 }
-
-console.log(obj['details']())  // object declare with function with ES6th 
+console.log(obj);
+console.log(obj['details']())  // object declare with function with ES6th version 
 // console.log(obj.name) // argument declare
+*/
+
+/*
+// ----------- 6th Method object with function declare Method with ES6 version <==(obj['details shows']())==> function Method -----------
+let n = "student";
+let  obj = {
+// as aproperty name assign
+    [n + "_name"] : "Irfan Ramzan",
+    course : "javascript",
+  'details shows'() {
+       return `This is Name "${this.student_name}" course name "${this.course}"`
+   }    
+}
+console.log(obj);
+                // function name
+console.log(obj['details shows']())  // object declare with function
+*/
+
+/*
+// ------------------8th Method Function delare Method --------
+//   let fullname = fname + " " +lname;
+
+let fname = "Muhammad";
+let lname = "irfan";
+let course = "Js";
+
+function students(fname,lname,course){
+    // ----------2nd Method declare
+    let fullname = fname + " " +lname;
+    return {fullname, course};
+}
+// object declaare in function
+console.log(students(fname, lname, course));
+console.log(typeof students(fname, lname, course));
+*/
+
+/*
+
+// ------------------9th Method Function delare Method --------
+// let s = students(fname, lname, course);
+// console.log(s.fullname, s.course);
+
+let fname = "Muhammad";
+let lname = "irfan";
+let course = "Js";
+
+function students(fname,lname,course){
+    // ----------2nd Method declare
+    let fullname = fname + " " +lname;
+    return {fullname, course};
+}
+// -------------property name delare
+let s = students(fname, lname, course);
+console.log(s.fullname, s.course);
+// console.log(typeof students(name, course));
+*/
